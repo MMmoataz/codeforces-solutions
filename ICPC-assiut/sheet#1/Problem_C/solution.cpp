d2490f8e5d4a621cb51b825151dcdfd6d3a21a7a
mmoataz <mohamedmoatazshatta@gmail.com>
2025-01-04 21:59:09 +0200

    added problem c

---
+++ ICPC-assiut/sheet#1/Problem_C/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x, y;
+
+    std::cin >> x >> y;
+
+    std::cout << x << " + " << y << " = " << x + y << "\n";
+    std::cout << x << " * " << y << " = " << x * y << "\n";
+    std::cout << x << " - " << y << " = " << x - y << "\n";
+
+}
